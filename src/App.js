b938965a14e8668374c67bde5e5173e24c404c21
Emam96 <emamsh990@gmail.com>
2021-08-08 17:14:49 +0300

    Complete 01

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
-
+import Footer from './components/Footer';
+import Main from './components/Main';
 class App extends React.Component {
   render() {
     return (
 
-      <>
-        <h1>My first react app</h1>
-        <p>hello from 301d29 students</p>
+<>
+      <Header/>
+      <Main/>
+      <Footer/>
+</>
 
-        <Header/>
-        
-        
-      </>
+
     );
 
   }
