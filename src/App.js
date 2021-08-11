9be224e5af872fa67b89f2197f9e2e4d97ca9ab5
Emam96 <emamsh990@gmail.com>
2021-08-11 12:12:19 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
 /* eslint-disable array-callback-return */
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Main from './components/Main';
-import SelectedBeast  from "./components/SelectedBeast";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import SelectedBeast from "./components/SelectedBeast";
 import dataForMap from "./components/dataForMap.json";
 
-
-
-
-
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       show: false,
-      beastList:  {}
+      beastList: {},
     };
   }
 
-///////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////
 
-  showModal = title => {
-    let beasts = dataForMap.find( beast => {
+  showModal = (title) => {
+    let beasts = dataForMap.find((beast) => {
       if (beast.title === title) {
         return beast;
       }
     });
 
-
-    ///////////////////////////////////////////////////////////////
-    
-    
     this.setState({
       beastList: beasts,
       show: true,
     });
   };
 
-
-
-/////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////
 
   handleClose = () => {
     this.setState({
@@ -49,30 +38,22 @@ class App extends React.Component {
       show: false,
     });
   };
-
-//////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////
 
   render() {
     return (
-
-<>
-      <Header/>
-      <Main showModal={this.showModal} />
-      <SelectedBeast show={this.state.show} handleClose={this.handleClose} beastList={this.state.beastList} />
-      <Footer/>
-</>
-
-
-
-
-
+      <>
+        <Header />
+        <Main showModal={this.showModal} />
+        <SelectedBeast
+          show={this.state.show}
+          handleClose={this.handleClose}
+          beastList={this.state.beastList}
+        />
+        <Footer />
+      </>
     );
-
   }
 }
 
 export default App;
-
-
-
-
